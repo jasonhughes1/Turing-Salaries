aaff043f1431677c1bd0cd05dd6c33b93299cd3d
Jason Hughes <jasonhughes088@gmail.com>
2018-02-14 14:26:39 -0700

    Router up and runnign

---
+++ turing-salaries/client/src/Components/CreateSalary/CreateSalary.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CreateSalary extends Component {
+  render() {
+    return (
+      <div>CreateSalary Lives Here</div>
+    )
+  }
+}
+
+export default CreateSalary;
--- turing-salaries/client/src/Components/NavBar/NavBar.js
+++ turing-salaries/client/src/Components/NavBar/NavBar.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class NavBar extends Component {
   render() {
+    
     return (
-      <div>NavBAR</div>
+      <div className='nav-bar'>
+    <h1 className='main-header'>Turing Salaries</h1>
+    <NavLink className='nav' exact to='/'>Home</NavLink>
+    <NavLink className='nav' to='/viewsalary'>ViewSalary</NavLink>
+    <NavLink className='nav' to='/createsalary'>CreateSalary</NavLink>
+  </div>
     )
   }
 }
