774741a9e6fb88aad91b29ab49117d47f382f1cb
Jason Hughes <jasonhughes088@gmail.com>
2018-02-15 13:56:53 -0700

    move links out ofnav bar and style header

--- turing-salaries/client/src/Components/Home/Home.js
+++ turing-salaries/client/src/Components/Home/Home.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './Home.css';
+import { NavLink } from 'react-router-dom';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <div className='image-container'>    
-        </div>
+      <div className='home-container'>
+        <div className='link-container'>
+        <NavLink className='nav' to='/viewsalary'>ViewSalary</NavLink>
+        <NavLink className='nav' to='/createsalary'>CreateSalary</NavLink>
+      </div>
+        <h1>HOME PAGE</h1>
       </div>
     )
   }
--- turing-salaries/client/src/Components/NavBar/NavBar.js
+++ turing-salaries/client/src/Components/NavBar/NavBar.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
 import './NavBar.css';
 
 class NavBar extends Component {
   render() {
-    
+
     return (
       <div className='nav-bar'>
     <div className="background-image">image</div>
     <h1 className='main-header'>Turing Salaries</h1>
-    <NavLink className='nav' exact to='/'>Home</NavLink>
-    <NavLink className='nav' to='/viewsalary'>ViewSalary</NavLink>
-    <NavLink className='nav' to='/createsalary'>CreateSalary</NavLink>
+    <div className='link-container'>
+  </div>
   </div>
     )
   }
