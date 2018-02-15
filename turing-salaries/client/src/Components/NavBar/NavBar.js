21e4f44fa8a6aba503b24405687d698dcc0fb075
Pat Neel <patrick.neel@colorado.edu>
2018-02-14 19:09:58 -0700

    nav-bar styling

--- turing-salaries/client/src/Components/NavBar/NavBar.js
+++ turing-salaries/client/src/Components/NavBar/NavBar.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import './NavBar.css';
 
 class NavBar extends Component {
   render() {
     
     return (
       <div className='nav-bar'>
+    <div className="background-image">image</div>
     <h1 className='main-header'>Turing Salaries</h1>
     <NavLink className='nav' exact to='/'>Home</NavLink>
     <NavLink className='nav' to='/viewsalary'>ViewSalary</NavLink>
