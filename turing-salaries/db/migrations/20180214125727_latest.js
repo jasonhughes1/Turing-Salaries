ec3946053bb3f39109251904ca5da2147079acc5
Pat Neel <patrick.neel@colorado.edu>
2018-02-17 14:52:46 -0700

    added migrations and formatted data

--- turing-salaries/db/migrations/20180214125727_latest.js
+++ turing-salaries/db/migrations/20180214125727_latest.js
@@ -1,24 +1,33 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('alumni', function(table) {
+    knex.schema.createTable('alumnus', function(table) {
       table.increments('id').primary();
-      table.string('company');
-      table.string('salary');
-      table.string('location');
-      table.string('offer_accepted_date');
-      table.string('graduating_cohort');
-      table.string('program');
+      table.string('original_cohort');
+      table.date('original_start_date');
+      table.string('final_cohort');
+      table.date('grad_date');
 
+    }),
 
+    knex.schema.createTable('offers', function(table) {
+      table.increments('id').primary();
+      table.date('accepted_date');
+      table.string('payment_type');
+      table.decimal('payment_rate');
+      table.decimal('annualized_salary');
+      table.string('title');
+      table.integer('alumnus_id').unsigned();
+      table.foreign('alumnus_id').references('alumnus.id')
 
-      table.timestamps(true, true);
     })
+
   ])
 };
 
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('alumni') 
+    knex.schema.dropTable('alumnus');
+    knex.schema.dropTable('offers')
   ]);
 };
