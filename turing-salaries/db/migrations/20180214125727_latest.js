7a48f86f85b2fbe62fed83950dbd2b8adbb1dcea
Jason Hughes <jasonhughes088@gmail.com>
2018-02-19 18:48:07 -0700

    Get rid of offers table and simplify data in schema

--- turing-salaries/db/migrations/20180214125727_latest.js
+++ turing-salaries/db/migrations/20180214125727_latest.js
@@ -6,26 +6,18 @@ exports.up = function(knex, Promise) {
       table.date('original_start_date');
       table.string('final_cohort');
       table.date('grad_date');
-    }),
-
-    knex.schema.createTable('offers', function(table) {
-      table.increments('id').primary();
       table.date('accepted_date');
       table.integer('days_to_offer')
       table.string('payment_type');
       table.decimal('payment_rate', null);
       table.decimal('annualized_salary', null);
       table.string('title');
-      table.integer('alumni_id').unsigned();
-      table.foreign('alumni_id').references('alumni.id');
     })
   ])
 };
 
-
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('offers'),
     knex.schema.dropTable('alumni')
   ]);
 };
--- turing-salaries/db/seeds/dev/salaries.js
+++ turing-salaries/db/seeds/dev/salaries.js
@@ -1,46 +1,31 @@
 const alumniData = require('../../../data/2016-2017-alumni.js');
-const offersData = require('../../../data/2016-2017-offers.js');
 
 
 const createAlumni = (knex, alumni) => {
+  console.log(alumni);
   return knex('alumni').insert({
     original_cohort: alumni.original_cohort,
     original_start_date: alumni.original_start_date,
     final_cohort: alumni.final_cohort,
-    grad_date: alumni.grad_date
-  }, 'id')
-  .then(alumniId => {
-    let offerPromises = [];
-
-    offersData.forEach(offers => {
-      offerPromises.push(
-        createOffer(knex, {
-          accepted_date: offers.accepted_date,
-          days_to_offer: offers.days_to_offer,
-          payment_type: offers.payment_type,
-          payment_rate: offers.payment_rate,
-          annualized_salary: offers.annualized_salary,
-          title: offers.title,
-          alumni_id: alumniId[0]
-        })
-      )
-    });
-    return Promise.all(offerPromises);
+    grad_date: alumni.grad_date,
+    accepted_date: alumni.accepted_date,
+    days_to_offer: alumni.days_to_offer,
+    payment_type: alumni.payment_type,
+    payment_rate: alumni.payment_rate,
+    annualized_salary: alumni.annualized_salary,
+    title: alumni.title
   })
 };
 
-const createOffer = (knex, offer) => {
-  return knex('offers').insert(offer);
-};
 
 
 exports.seed = (knex, Promise) => {
-  return knex('offers').del()
+  return knex('alumni').del()
   .then(() => knex('alumni').del())
   .then(() => {
     let alumniPromises = [];
 
-    alumniData.forEach(alumni => {
+    alumniData.forEach((alumni) => {
       alumniPromises.push(createAlumni(knex, alumni));
     });
     return Promise.all(alumniPromises);
