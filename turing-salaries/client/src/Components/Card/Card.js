3a45955c0c7a016aed373aad6afbb3b17bdfb044
Pat Neel <patrick.neel@colorado.edu>
2018-02-21 18:52:10 -0700

    fixed card styling

--- turing-salaries/client/src/Components/Card/Card.js
+++ turing-salaries/client/src/Components/Card/Card.js
@@ -5,16 +5,16 @@ import './Card.css';
 const Card = (props) => {
   return (
     <div className='card'>
-      <p>Job Title: {props.title}</p>
-      <p>Salary: ${props.paymentRate}</p>
-      <p>Annualized Salary: {props.annualizedSalary}</p>
-      <p>Payment Type: {props.paymentType}</p>
-      <p>Offer Accepted: {props.acceptedDate.slice(0, -14)}</p>
-      <p>Days to Offer: {props.daysToOffer}</p>
-      <p>Graduation Date: {props.gradDate.slice(0, -14)}</p>
-      <p>Original Cohort: {props.originalCohort}</p>
-      <p>Final Cohort: {props.finalCohort}</p>
-      <p>Turing Start Date: {props.originalStartDate.slice(0, -14)}</p>
+      <p><span className="card-titles">Job Title:</span> {props.title}</p>
+      <p><span className="card-titles">Salary:</span> ${props.paymentRate}</p>
+      <p><span className="card-titles">Annualized Salary:</span> {props.annualizedSalary}</p>
+      <p><span className="card-titles">Payment Type:</span> {props.paymentType}</p>
+      <p><span className="card-titles">Offer Accepted:</span> {props.acceptedDate.slice(0, -14)}</p>
+      <p><span className="card-titles">Days to Offer:</span> {props.daysToOffer}</p>
+      <p><span className="card-titles">Graduation Date:</span> {props.gradDate.slice(0, -14)}</p>
+      <p><span className="card-titles">Original Cohort:</span> {props.originalCohort}</p>
+      <p><span className="card-titles">Final Cohort:</span> {props.finalCohort}</p>
+      <p><span className="card-titles">Turing Start Date:</span> {props.originalStartDate.slice(0, -14)}</p>
     </div>
   )
 }
