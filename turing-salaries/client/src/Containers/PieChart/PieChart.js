5ee0e3a1d83a5a3c83c8433c4d3d11d28059129f
Pat Neel <patrick.neel@colorado.edu>
2018-02-23 08:56:53 -0700

    pie chart sort of

--- turing-salaries/client/src/Containers/PieChart/PieChart.js
+++ turing-salaries/client/src/Containers/PieChart/PieChart.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { VictoryPie, VictoryLabel } from 'victory';
+import { VictoryPie, VictoryLabel, VictoryContainer } from 'victory';
 import { connect } from 'react-redux';
 
 class PieChart extends Component {
@@ -18,25 +18,29 @@ class PieChart extends Component {
         accu[values.title]++
         return accu;
       }, {})
-      console.log(data);
 
     return (
-      <VictoryPie
-        
-                height={150}
-                 width={150}
-                 style={{
-
-       labels: {
-         fontSize: 2, padding: 5
-       }
-     }}
-                  data={Object.values(data)}
-                labels={Object.keys(data)}
-        labelComponent={<VictoryLabel
-                 angle={40}/>}
-            colorScale={["tomato", "orange", "gold", "cyan", "navy"]}
-      />
+        <svg viewBox ='0 0 400 400'>
+          <VictoryPie
+              standalone={false}
+              height={400}
+              width={400}
+              labelRadius={160}
+              innerRadius={50}
+              padAngle={5}
+              startAngle={0}
+              style={{
+                  labels: {
+                    fontSize: 10, padding: 16, fill: 'white'
+                   }
+                 }}
+              data={Object.values(data)}
+              labels={Object.keys(data)}
+              labelComponent={<VictoryLabel
+                  angle={80}/>}
+                  colorScale={["#53599A", "#6D9DC5", "#20A39E", "#F4D03F", "#EF5B5B", "#007AFF", "#ED63D1", "#C97064", "#FF8800", "#238966"]}
+          />
+        </svg>
       );
     }
   }
