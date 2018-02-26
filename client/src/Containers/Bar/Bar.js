e4a5e428ac8db7f5017c472723c17fbab8ffb179
Pat Neel <patrick.neel@colorado.edu>
2018-02-26 13:16:20 -0700

    got bar graph working

--- client/src/Containers/Bar/Bar.js
+++ client/src/Containers/Bar/Bar.js
@@ -6,43 +6,41 @@ import { connect } from 'react-redux';
 
 class Bar extends Component {
 
-  //
-  // dataCleaner(data) {
-  //   let cohort;
-  //   if(data.length) {
-  //     cohort = data[0].filter(item => item.final_cohort === '1507')
-  //   }
-  //   return cohort
-  // }
-
   render() {
-  //   if(!this.props.alumni.length) {
-  //     return (
-  //       <div>Loading</div>
-  //     )
-  //   } else {
-  //
-  //
-  // let alums = this.dataCleaner(this.props.alumData)
-  // let alum = alums.map(salary => parseInt(salary.annualized_salary))
-  // console.log(alum);
+    if(!this.props.alumni.length) {
+      return (
+        <div>Loading</div>
+      )
+    } else {
+   let data = this.props.alumni[0]
+   let offerDays = data.map(days => days.days_to_offer)
+  
+   let beforeGrad = offerDays.filter(small => small <= 0)
+   console.log(beforeGrad.length)
+   let smallDays = offerDays.filter(small => small > 0 && small <= 30)
+   console.log(smallDays.length)
+   let mediumDays = offerDays.filter(small => small > 30 && small <= 60)
+   console.log(mediumDays.length)
+   let bigDays = offerDays.filter(small => small > 60 && small <= 90)
+   console.log(bigDays.length)
+   let largeDays = offerDays.filter(small => small > 90 && small <= 400)
+   console.log(largeDays.length)
+   
+    
     let options = {
 
       chart: {
         type: 'column'
     },
     title: {
-        text: 'Browser market shares. January, 2015 to May, 2015'
-    },
-    subtitle: {
-        text: 'Click the columns to view versions. Source: <a href="http://netmarketshare.com">netmarketshare.com</a>.'
+        text: 'Days To Offer'
     },
     xAxis: {
         type: 'category'
     },
     yAxis: {
         title: {
-            text: 'Total percent market share'
+            text: 'Number of Alumni'
         }
 
     },
@@ -65,218 +63,26 @@ class Bar extends Component {
     },
 
     series: [{
-        name: 'Brands',
+        name: 'Days to Offer',
         colorByPoint: true,
         data: [{
-            name: 'Microsoft Internet Explorer',
-            y: 56.33,
-            drilldown: 'Microsoft Internet Explorer'
+            name: 'Hired Before Graduation',
+            y: beforeGrad.length,
         }, {
-            name: 'Chrome',
-            y: 24.03,
-            drilldown: 'Chrome'
+            name: '0-30 Days',
+            y: smallDays.length,
         }, {
-            name: 'Firefox',
-            y: 10.38,
-            drilldown: 'Firefox'
+            name: '31-60 Days',
+            y: mediumDays.length,
         }, {
-            name: 'Safari',
-            y: 4.77,
-            drilldown: 'Safari'
+            name: '61-90 Days',
+            y: bigDays.length,
         }, {
-            name: 'Opera',
-            y: 0.91,
-            drilldown: 'Opera'
-        }, {
-            name: 'Proprietary or Undetectable',
-            y: 0.2,
-            drilldown: null
+            name: '100 + Days',
+            y: largeDays.length,
+
         }]
     }],
-    drilldown: {
-        series: [{
-            name: 'Microsoft Internet Explorer',
-            id: 'Microsoft Internet Explorer',
-            data: [
-                [
-                    'v11.0',
-                    24.13
-                ],
-                [
-                    'v8.0',
-                    17.2
-                ],
-                [
-                    'v9.0',
-                    8.11
-                ],
-                [
-                    'v10.0',
-                    5.33
-                ],
-                [
-                    'v6.0',
-                    1.06
-                ],
-                [
-                    'v7.0',
-                    0.5
-                ]
-            ]
-        }, {
-            name: 'Chrome',
-            id: 'Chrome',
-            data: [
-                [
-                    'v40.0',
-                    5
-                ],
-                [
-                    'v41.0',
-                    4.32
-                ],
-                [
-                    'v42.0',
-                    3.68
-                ],
-                [
-                    'v39.0',
-                    2.96
-                ],
-                [
-                    'v36.0',
-                    2.53
-                ],
-                [
-                    'v43.0',
-                    1.45
-                ],
-                [
-                    'v31.0',
-                    1.24
-                ],
-                [
-                    'v35.0',
-                    0.85
-                ],
-                [
-                    'v38.0',
-                    0.6
-                ],
-                [
-                    'v32.0',
-                    0.55
-                ],
-                [
-                    'v37.0',
-                    0.38
-                ],
-                [
-                    'v33.0',
-                    0.19
-                ],
-                [
-                    'v34.0',
-                    0.14
-                ],
-                [
-                    'v30.0',
-                    0.14
-                ]
-            ]
-        }, {
-            name: 'Firefox',
-            id: 'Firefox',
-            data: [
-                [
-                    'v35',
-                    2.76
-                ],
-                [
-                    'v36',
-                    2.32
-                ],
-                [
-                    'v37',
-                    2.31
-                ],
-                [
-                    'v34',
-                    1.27
-                ],
-                [
-                    'v38',
-                    1.02
-                ],
-                [
-                    'v31',
-                    0.33
-                ],
-                [
-                    'v33',
-                    0.22
-                ],
-                [
-                    'v32',
-                    0.15
-                ]
-            ]
-        }, {
-            name: 'Safari',
-            id: 'Safari',
-            data: [
-                [
-                    'v8.0',
-                    2.56
-                ],
-                [
-                    'v7.1',
-                    0.77
-                ],
-                [
-                    'v5.1',
-                    0.42
-                ],
-                [
-                    'v5.0',
-                    0.3
-                ],
-                [
-                    'v6.1',
-                    0.29
-                ],
-                [
-                    'v7.0',
-                    0.26
-                ],
-                [
-                    'v6.2',
-                    0.17
-                ]
-            ]
-        }, {
-            name: 'Opera',
-            id: 'Opera',
-            data: [
-                [
-                    'v12.x',
-                    0.34
-                ],
-                [
-                    'v28',
-                    0.24
-                ],
-                [
-                    'v27',
-                    0.17
-                ],
-                [
-                    'v29',
-                    0.16
-                ]
-            ]
-        }]
-    }
 }
     return(
       <div>
@@ -285,9 +91,10 @@ class Bar extends Component {
           options={options}
          />
       </div>
-    )
+      )
+    }
   }
-  }
+}
 
 
 export const mapStateToProps = (store) => {
