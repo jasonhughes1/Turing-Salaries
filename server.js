b288ac67655a5eb20c7813f2fa55f0d61b46c99d
Jason Hughes <jasonhughes088@gmail.com>
2018-02-26 11:24:20 -0700

    add app set

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.set('port', process.env.PORT || 5000);
+
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('./knexfile')[environment];
 const database = require('knex')(configuration);
