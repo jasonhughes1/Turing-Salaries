0eb07b017a8faeadcde7ce1b91073808d628db62
Jason Hughes <jasonhughes088@gmail.com>
2018-02-26 20:05:47 -0700

    fix styles for view pie

--- client/src/Containers/ViewPie/ViewPie.js
+++ client/src/Containers/ViewPie/ViewPie.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Pie from '../Pie/Pie';
+import './ViewPie.css';
 
 class ViewPie extends Component {
 
   render() {
     return (
-      <div className='view-salary-container'>
+      <div className='view-pie-container'>
         <div className='link-container-view'>
         <NavLink className='nav' exact to='/'>Home</NavLink>
       </div>
         <h2 className='view-salary-title'>Turing grads have been accepting offers with these job titles</h2>
           <Pie />
+          <div className='place-holder'></div>
       </div>
     )
   }
